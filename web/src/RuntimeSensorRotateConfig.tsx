8f1b839df6cb7d8385399a27fc028e0163a32655
cormoran <cormoran707@gmail.com>
2026-02-11 14:39:10 +0900

    Fix lint error

--- web/src/RuntimeSensorRotateConfig.tsx
+++ web/src/RuntimeSensorRotateConfig.tsx
@@ -33,6 +33,7 @@ export function RuntimeSensorRotateConfig() {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const subsystem = useMemo(
     () => zmkApp?.findSubsystem(SUBSYSTEM_IDENTIFIER),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [zmkApp?.state.customSubsystems]
   );
 
